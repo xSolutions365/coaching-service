14a75ca1128112587f52ab4f621bf9afa0af9a9d
emilyharper155 <emily.harper@createfuture.com>
2025-11-25 13:11:46 +0000

    Add files via upload

---
+++ src/main/kotlin/com/createfuture/coaching/service/CoachingServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.createfuture.coaching.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoachingServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoachingServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/createfuture/coaching/service/CoachingServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.createfuture.coaching.service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoachingServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
