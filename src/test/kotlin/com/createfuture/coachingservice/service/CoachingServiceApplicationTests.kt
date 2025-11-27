cd268d70bee27afe83264a6d75d8755842c06530
Alan Lovell <alan.lovell@createfuture.com>
2025-11-27 14:07:04 +0000

    CP-6: Move CSApplication to "correct" place

--- src/test/kotlin/com/createfuture/coachingservice/service/CoachingServiceApplicationTests.kt
+++ src/test/kotlin/com/createfuture/coachingservice/service/CoachingServiceApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.createfuture.coaching.service
+package com.createfuture.coachingservice.service
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
