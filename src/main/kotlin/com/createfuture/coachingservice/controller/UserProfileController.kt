8b8cb793bc1ff058cbf60ac00a58dd91398d5f9b
Alan Lovell <alan.lovell@createfuture.com>
2025-12-03 10:37:26 +0000

    CP-6: Add POST seed endpoint to UserProfileController

--- src/main/kotlin/com/createfuture/coachingservice/controller/UserProfileController.kt
+++ src/main/kotlin/com/createfuture/coachingservice/controller/UserProfileController.kt
@@ -1,11 +1,18 @@
 package com.createfuture.coachingservice.controller
 
 import com.createfuture.coachingservice.service.UserProfileService
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
+import java.util.UUID
 
 @RestController
 @RequestMapping("/api/v1/user")
 class UserProfileController(private val userProfileService: UserProfileService) {
+
+    @PostMapping("/seed")
+    fun seedUsers(count: Int): List<UUID> {
+        return userProfileService.seed(count);
+    }
 
 }
