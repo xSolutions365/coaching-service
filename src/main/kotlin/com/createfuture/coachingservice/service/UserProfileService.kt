31a45de655724b470a8bdd6fdf82f762c50c8db4
Alan Lovell <alan.lovell@createfuture.com>
2025-12-03 10:32:45 +0000

    CP-6: Add UserProfile cache seeding functions

--- src/main/kotlin/com/createfuture/coachingservice/service/UserProfileService.kt
+++ src/main/kotlin/com/createfuture/coachingservice/service/UserProfileService.kt
@@ -3,6 +3,7 @@ package com.createfuture.coachingservice.service
 import com.createfuture.coachingservice.model.UserProfile
 import org.springframework.stereotype.Service
 import java.util.UUID
+import java.util.UUID.randomUUID
 
 @Service
 class UserProfileService {
@@ -10,4 +11,44 @@ class UserProfileService {
     // TODO: This should interact with persistence, somewhere
     private var userCache = mutableMapOf<UUID, UserProfile>()
 
+    /* SEED UTILS */
+
+    fun getRandomName(): String {
+        val namesStream = javaClass.classLoader.getResourceAsStream("names.txt")
+        val names = namesStream?.bufferedReader()?.readLines() ?: listOf("Random User")
+
+        val surnamesStream = javaClass.classLoader.getResourceAsStream("surnames.txt")
+        val surnames = surnamesStream?.bufferedReader()?.readLines() ?: listOf("Random User")
+
+        return "${names.random()} ${surnames.random()}"
+    }
+
+
+    fun getRandomProfile(): UserProfile {
+        val name = getRandomName()
+        val username = name.lowercase().replace(" ", ".")
+
+        return UserProfile(
+            id = randomUUID(),
+            username = username,
+            preferredName = name,
+            slackUsername = "@$username",
+            email = "$username@createfuture.com"
+        )
+    }
+
+    fun seed(count: Int): List<UUID> {
+
+        val tempUserCache = mutableMapOf<UUID, UserProfile>()
+
+        for (i in 1..count) {
+            val user = getRandomProfile()
+            tempUserCache[user.id] = user
+        }
+
+        userCache.putAll(tempUserCache)
+        return tempUserCache.keys.toList()
+    }
+
+    /* END OF SEED UTILS */
 }
