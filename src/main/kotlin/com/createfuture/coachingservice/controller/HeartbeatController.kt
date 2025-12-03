837b64730364a71def006a2f9803e572ab91382e
Alan Lovell <alan.lovell@createfuture.com>
2025-12-03 10:24:58 +0000

    CP-6: Correct heartbeat controller route

--- src/main/kotlin/com/createfuture/coachingservice/controller/HeartbeatController.kt
+++ src/main/kotlin/com/createfuture/coachingservice/controller/HeartbeatController.kt
@@ -1,11 +1,13 @@
-package com.createfuture.coaching.controller
+package com.createfuture.coachingservice.controller
 
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/api/v1/heartbeat")
 class HeartbeatController {
-    @GetMapping("/heartbeat")
+    @GetMapping("/")
     fun heartbeat(): HttpStatus = HttpStatus.OK
 }
