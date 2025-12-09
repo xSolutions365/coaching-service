bad5e682c3457ecc01d3763ae7a6eaf4e77c91d9
Alan Lovell <alan.lovell@createfuture.com>
2025-12-09 09:59:05 +0000

    CP-6: Add POST /user to UserProfileController

--- src/main/kotlin/com/createfuture/coachingservice/controller/UserProfileController.kt
+++ src/main/kotlin/com/createfuture/coachingservice/controller/UserProfileController.kt
@@ -1,11 +1,14 @@
 package com.createfuture.coachingservice.controller
 
+import com.createfuture.coachingservice.model.NewUserProfile
+import com.createfuture.coachingservice.model.UserProfile
 import com.createfuture.coachingservice.model.response.UserProfileApiResponseEntity
 import com.createfuture.coachingservice.model.toApiResponseEntity
 import com.createfuture.coachingservice.service.UserProfileService
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
 import java.util.UUID
@@ -13,6 +16,14 @@ import java.util.UUID
 @RestController
 @RequestMapping("/api/v1/user")
 class UserProfileController(private val userProfileService: UserProfileService) {
+
+    @PostMapping
+    fun createUserProfile(newUser: NewUserProfile): UserProfileApiResponseEntity {
+        println("Creating new user profile")
+
+        val user = userProfileService.createUser(newUser)
+        return user.toApiResponseEntity()
+    }
 
     @GetMapping("/{userId}")
     fun getUserProfile(@PathVariable userId: UUID): UserProfileApiResponseEntity {
